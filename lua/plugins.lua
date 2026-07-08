87750fe88bd7f39861a8eea831c14ea30cdc606a
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-07-08 19:40:13 +0800

    fix(plugins): venv-selector telescope 依赖改用 snacks.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,7 +50,7 @@ return {
 	{
 		"linux-cultist/venv-selector.nvim",
 		dependencies = {
-			{ "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
+			{ "nvim-telescope/telescope.nvim", version = "*", dependencies = { "folke/snacks.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
 		},
 		ft = "python", -- Load when opening Python files
 		keys = { { ",v", "<cmd>VenvSelect<cr>" } }, -- Open picker on keymap
