0442dfb84e399de37a8260fd102dfb2d10f48b96
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-07-08 15:49:14 +0800

    feat(plugins): 添加插件清单

---
+++ lua/plugins.lua
@@ -0,0 +1,50 @@
+return {
+	-- {
+	-- "folke/tokyonight.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- opts = {},
+	-- },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "hiphish/rainbow-delimiters.nvim" },
+	{
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	{ "wakatime/vim-wakatime", lazy = false },
+}
