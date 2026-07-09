05c200fd9a47c7c5982a034b42a7b45ca87f4d70
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-07-09 13:35:20 +0800

    feat(plugins): 添加 auto-session 会话管理

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -59,4 +59,16 @@ return {
 			search = {}, -- custom search definitions
 		},
 	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = { "~/", "~/Downloads", "/" },
+			-- log_level = 'debug',
+		},
+	},
 }
